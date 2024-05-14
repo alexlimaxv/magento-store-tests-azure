e5f3982ba529a2e56bc748fd905f80bb4648ba54
alexlimaxv <alexlimaxv@gmail.com>
2024-05-13 22:10:16 -0300

    Initial commit

---
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+
+describe('example to-do app', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('access login page', () => {
+    cy.log("ok")
+  })
+
+})
